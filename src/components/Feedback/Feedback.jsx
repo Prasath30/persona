a9e288507cd11a090637d43213d88142b54f3a60
Prasath30 <prasathbalasubramani@gmail.com>
2022-01-18 14:28:14 +0530

    added-feedback-sec

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -16,9 +16,9 @@ const Feedback = () => {
                         <td>Did you felt better after the session ?</td>
                         <td>
                            	<div class="switch-field">
-                                <input type="radio" id="switch-one-radio-one" name="switch-one" value="yes"  />
+                                <input type="radio" className='feedback-yes' id="switch-one-radio-one" name="switch-one" value="yes"  />
                                 <label for="switch-one-radio-one">Yes</label>
-                                <input type="radio" id="switch-one-radio-two" name="switch-one" value="no" />
+                                <input type="radio" className='feedback-no' id="switch-one-radio-two" name="switch-one" value="no" />
                                 <label for="switch-one-radio-two">No</label>
                             </div>
                         </td>
@@ -27,9 +27,9 @@ const Feedback = () => {
                         <td>Have you gained more clarity on your problems?</td>
                         <td>
                         <div class="switch-field">
-                                <input type="radio" id="switch-two-radio-one" name="switch-two" value="yes" />
+                                <input type="radio" className='feedback-yes' id="switch-two-radio-one" name="switch-two" value="yes" />
                                 <label for="switch-two-radio-one">Yes</label>
-                                <input type="radio" id="switch-two-radio-two" name="switch-two" value="no" />
+                                <input type="radio" className='feedback-no'  id="switch-two-radio-two" name="switch-two" value="no" />
                                 <label for="switch-two-radio-two">No</label>
                             </div>
                         </td>
@@ -38,9 +38,9 @@ const Feedback = () => {
                         <td>Do you know strategies to handle your problems ?</td>
                         <td>
                             <div class="switch-field">
-                                <input type="radio" id="switch-three-radio-one" name="switch-three" value="yes" />
+                                <input type="radio" className='feedback-yes' id="switch-three-radio-one" name="switch-three" value="yes" />
                                 <label for="switch-three-radio-one">Yes</label>
-                                <input type="radio" id="switch-three-radio-two" name="switch-three" value="no" />
+                                <input type="radio" className='feedback-no'  id="switch-three-radio-two" name="switch-three" value="no" />
                                 <label for="switch-three-radio-two">No</label>
                             </div>
                         </td>
@@ -49,9 +49,9 @@ const Feedback = () => {
                         <td>Do you feel motivated to improve your well-being ?</td>
                         <td>
                             <div class="switch-field">
-                                <input type="radio" id="switch-four-radio-one" name="switch-four" value="yes" />
+                                <input type="radio" className='feedback-yes' id="switch-four-radio-one" name="switch-four" value="yes" />
                                 <label for="switch-four-radio-one">Yes</label>
-                                <input type="radio" id="switch-four-radio-two" name="switch-four" value="no" />
+                                <input type="radio" className='feedback-no'  id="switch-four-radio-two" name="switch-four" value="no" />
                                 <label for="switch-four-radio-two">No</label>
                             </div>
                         </td>
@@ -86,10 +86,10 @@ const Feedback = () => {
                         <td>Did you find the workshop topic to be relevant ?</td>
                         <td>
                            	<div class="switch-field">
-                                <input type="radio" id="switch-one-radio-one" name="switch-one" value="yes"  />
-                                <label for="switch-one-radio-one">Yes</label>
-                                <input type="radio" id="switch-one-radio-two" name="switch-one" value="no" />
-                                <label for="switch-one-radio-two">No</label>
+                                <input type="radio" className='feedback-yes' id="switch-five-radio-one" name="switch-five" value="yes"  />
+                                <label for="switch-five-radio-one">Yes</label>
+                                <input type="radio" className='feedback-no' id="switch-five-radio-two" name="switch-five" value="no" />
+                                <label for="switch-five-radio-two">No</label>
                             </div>
                         </td>
                         </tr>
@@ -97,10 +97,10 @@ const Feedback = () => {
                         <td>Do you now have the tools to tackle tough situations ?</td>
                         <td>
                         <div class="switch-field">
-                                <input type="radio" id="switch-two-radio-one" name="switch-two" value="yes" />
-                                <label for="switch-two-radio-one">Yes</label>
-                                <input type="radio" id="switch-two-radio-two" name="switch-two" value="no" />
-                                <label for="switch-two-radio-two">No</label>
+                                <input type="radio" className='feedback-yes' id="switch-six-radio-one" name="switch-six" value="yes" />
+                                <label for="switch-six-radio-one">Yes</label>
+                                <input type="radio" className='feedback-no'  id="switch-six-radio-two" name="switch-six" value="no" />
+                                <label for="switch-six-radio-two">No</label>
                             </div>
                         </td>
                         </tr>
@@ -108,10 +108,10 @@ const Feedback = () => {
                         <td>Did you gained good perspective on mental health ?</td>
                         <td>
                             <div class="switch-field">
-                                <input type="radio" id="switch-three-radio-one" name="switch-three" value="yes" />
-                                <label for="switch-three-radio-one">Yes</label>
-                                <input type="radio" id="switch-three-radio-two" name="switch-three" value="no" />
-                                <label for="switch-three-radio-two">No</label>
+                                <input type="radio" className='feedback-yes' id="switch-seven-radio-one" name="switch-seven" value="yes" />
+                                <label for="switch-seven-radio-one">Yes</label>
+                                <input type="radio" className='feedback-no' id="switch-seven-radio-two" name="switch-seven" value="no" />
+                                <label for="switch-seven-radio-two">No</label>
                             </div>
                         </td>
                         </tr>
@@ -119,10 +119,10 @@ const Feedback = () => {
                         <td>Did the workshop motivated you to work better ?</td>
                         <td>
                             <div class="switch-field">
-                                <input type="radio" id="switch-four-radio-one" name="switch-four" value="yes" />
-                                <label for="switch-four-radio-one">Yes</label>
-                                <input type="radio" id="switch-four-radio-two" name="switch-four" value="no" />
-                                <label for="switch-four-radio-two">No</label>
+                                <input type="radio" className='feedback-yes' id="switch-nine-radio-one" name="switch-nine" value="yes" />
+                                <label for="switch-nine-radio-one">Yes</label>
+                                <input type="radio" className='feedback-no' id="switch-nine-radio-two" name="switch-nine" value="no" />
+                                <label for="switch-nine-radio-two">No</label>
                             </div>
                         </td>
                         </tr>
